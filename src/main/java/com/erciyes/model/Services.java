27fb3bef5cbecbcfd4d990db37950b2026afed1c
ismailrecber <ismaailrecber@hotmail.com>
2024-11-24 17:40:36 +0300

    Services created

---
+++ src/main/java/com/erciyes/model/Services.java
@@ -0,0 +1,29 @@
+package com.erciyes.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Entity
+@Table(name = "services")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Services extends BaseEntity{
+
+    private String name;
+
+    private int duration;
+
+    private BigDecimal price;
+
+    @ManyToOne
+    private BarberShop barberShop;
+}
