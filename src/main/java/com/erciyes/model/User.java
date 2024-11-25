d102ce61d926c0f30f7f4816ec7bcb3a2d7cffd1
Batuhan Çakmak <batucakmak.jpg@gmail.com>
2024-11-25 13:17:30 +0300

    Update User.java

--- src/main/java/com/erciyes/model/User.java
+++ src/main/java/com/erciyes/model/User.java
@@ -4,10 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -16,9 +13,8 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Getter
-@Setter
 @Table(name = "user")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User extends BaseEntity implements UserDetails {
@@ -46,34 +42,6 @@ public class User extends BaseEntity implements UserDetails {
         return List.of();
     }
 
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 
 
 }
