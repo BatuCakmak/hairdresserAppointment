3620c006078d6ad949c31a0e5909a5d77f999475
ismailrecber <ismaailrecber@hotmail.com>
2024-11-27 16:09:57 +0300

    RefreshToken hataları düzenlendi

--- src/main/java/com/erciyes/repository/RefreshTokenRepository.java
+++ src/main/java/com/erciyes/repository/RefreshTokenRepository.java
@@ -2,8 +2,16 @@ package com.erciyes.repository;
 
 import com.erciyes.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
+    boolean findAllByUserId(Long id);
+
+    @Query("SELECT rt FROM RefreshToken rt WHERE rt.user.id = :userId")
+    Optional<RefreshToken> findByUserId(@Param("userId") Long userId);
 }
