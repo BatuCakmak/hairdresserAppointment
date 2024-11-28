a2abf16e31a246a3dda67e1522a35fd00087fc13
Baturenes17 <kilicbatur614@gmail.com>
2024-11-28 14:39:32 +0300

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -52,51 +52,50 @@ function LoginPage() {
             console.error("Kayıt sırasında bir hata oluştu:", error);
         }
     };
-};
 
 
 
-return (
-    <div className="main-div-login">
+    return (
+        <div className="main-div-login">
 
-        <Header />
+            <Header />
 
-        <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped}>
-            <div className="card">
-                <div className="card-front-left">
-                    <input onChange={handleChange} name="name" style={{ textTransform: "capitalize" }} className="input-box" type="text" placeholder="Name" />
-                    <input onChange={handleChange} name="surname" style={{ textTransform: "capitalize" }} className="input-box" type="text" placeholder="Surname" />
-                    <input onChange={handleChange} name="username" className="input-box" type="text" placeholder="Username" />
-                    <input onChange={handleChange} name="email" className="input-box" type="email" placeholder="Mail" />
-                    <input onChange={handleChange} name="password" className="input-box" type="password" placeholder="Password" />
+            <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped}>
+                <div className="card">
+                    <div className="card-front-left">
+                        <input onChange={handleChange} name="name" style={{ textTransform: "capitalize" }} className="input-box" type="text" placeholder="Name" />
+                        <input onChange={handleChange} name="surname" style={{ textTransform: "capitalize" }} className="input-box" type="text" placeholder="Surname" />
+                        <input onChange={handleChange} name="username" className="input-box" type="text" placeholder="Username" />
+                        <input onChange={handleChange} name="email" className="input-box" type="email" placeholder="Mail" />
+                        <input onChange={handleChange} name="password" className="input-box" type="password" placeholder="Password" />
 
-                    <div>
-                        <button onClick={handleSignUp} type="button" class="btn btn-outline-primary">Sign-Up</button>
+                        <div>
+                            <button onClick={handleSignUp} type="button" class="btn btn-outline-primary">Sign-Up</button>
+                        </div>
+                    </div>
+
+                    <div className="card-front-right">
+                        <h2 onClick={flipCard} className="login-text" >Login</h2>
                     </div>
                 </div>
 
-                <div className="card-front-right">
-                    <h2 onClick={flipCard} className="login-text" >Login</h2>
-                </div>
-            </div>
+                <div className="card card-back">
+                    <div className="card-back-left">
+                        <h2 onClick={flipCard} className="signup-text" >Sign-Up</h2>
+                    </div>
 
-            <div className="card card-back">
-                <div className="card-back-left">
-                    <h2 onClick={flipCard} className="signup-text" >Sign-Up</h2>
-                </div>
+                    <div className="card-back-right">
+                        <input className="input-box" type="text" placeholder="Username" />
+                        <input className="input-box" type="password" placeholder="Password" />
 
-                <div className="card-back-right">
-                    <input className="input-box" type="text" placeholder="Username" />
-                    <input className="input-box" type="password" placeholder="Password" />
-
-                    <div>
-                        <button type="button" class="btn btn-outline-primary">Login</button>
+                        <div>
+                            <button type="button" class="btn btn-outline-primary">Login</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </ReactCardFlip>
-    </div>
-);
-
+            </ReactCardFlip>
+        </div>
+    );
+}
 
 export default LoginPage;
